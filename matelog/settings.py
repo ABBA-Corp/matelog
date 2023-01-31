9e0e481eaaddbbaac3540ae96eaa76fe7f616e9b
MSD <dadamuhames@gmail.com>
2023-01-31 15:23:49 +0500

    First

--- matelog/settings.py
+++ matelog/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages', 'main',
+    'django.contrib.messages', 'main', 'corsheaders',
     'django.contrib.staticfiles', 'rest_framework',
     'ckeditor_uploader', 'easy_thumbnails', 'admins'
 ]
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -183,3 +184,20 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,
     'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
 }
+
+
+# If this is used then `CORS_ALLOWED_ORIGINS` will not have any effect
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+]
+
+
+#CORS_ALLOW_HEADERS = default_headers + (
+#    'Access-Control-Allow-Headers',
+#    'Access-Control-Allow-Credentials',
+#    'Access-Control-Allow-Origin',
+#)
+
+
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_ALLOW_ALL = True
