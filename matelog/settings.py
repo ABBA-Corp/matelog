137063686ff4dfe98bb6df114d52381eab193cab
MSD <dadamuhames@gmail.com>
2023-01-31 18:06:51 +0500

    First

--- matelog/settings.py
+++ matelog/settings.py
@@ -44,7 +44,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    'matelog.cors_midlware.CustomCorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
