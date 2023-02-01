ce4296b119f3a73b99439bc269b835256157f1dc
MSD <dadamuhames@gmail.com>
2023-02-01 14:46:31 +0500

    First

--- matelog/settings.py
+++ matelog/settings.py
@@ -45,8 +45,8 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'matelog.cors_midlware.CustomCorsMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -184,3 +184,7 @@ REST_FRAMEWORK = {
 
 #CORS_ALLOW_CREDENTIALS = True
 #CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = "*"
+CORS_ORIGIN_WHITELIST = [
+    'http://127.0.0.1:5173',
+]
