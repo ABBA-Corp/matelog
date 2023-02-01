6119a70c794a9aa674e72b5f57adb28dbf2d08e2
MSD <dadamuhames@gmail.com>
2023-02-01 14:51:15 +0500

    First

--- matelog/settings.py
+++ matelog/settings.py
@@ -186,5 +186,5 @@ REST_FRAMEWORK = {
 #CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_HEADERS = "*"
 CORS_ORIGIN_WHITELIST = [
-    'http://127.0.0.1:5173', "http://localhost:3000", "https://meta-logistic.vercel.app"
+    'http://127.0.0.1:5173', "http://localhost:3000"
 ]
