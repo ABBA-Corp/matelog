8319abdd217570c8c2242f48a66fbfdfb15a2233
MSD <dadamuhames@gmail.com>
2023-02-01 14:36:34 +0500

    First

--- matelog/cors_midlware.py
+++ matelog/cors_midlware.py
@@ -11,10 +11,10 @@ class CustomCorsMiddleware:
         # the view (and later middleware) are called.
 
         response = self.get_response(request)
-        response["Access-Control-Allow-Origin"] = "*"
-        response["Access-Control-Allow-Headers"] = "multipart/form-data, Access-Control-Allow-Origin"
-        response['Access-Control-Allow-Methods'] = "GET, HEAD, PUT, PATCH, POST, DELETE"
-        response["Access-Control-Allow-Credentials"] = '*'
+        # response["Access-Control-Allow-Origin"] = "*"
+        # response["Access-Control-Allow-Headers"] = "multipart/form-data, Access-Control-Allow-Origin"
+        # response['Access-Control-Allow-Methods'] = "GET, HEAD, PUT, PATCH, POST, DELETE"
+        # response["Access-Control-Allow-Credentials"] = '*'
 
         print(response.headers)
 
