2b2c84c6769c38bd329bc61f420b5573dc844052
MSD <dadamuhames@gmail.com>
2023-02-19 21:58:19 +0500

    fix translation search and translation gorup delete

--- admins/utils.py
+++ admins/utils.py
@@ -97,7 +97,7 @@ def search_translation(query, queryset):
         query = query.lower()
         for item in queryset:
             for lang in langs:
-                if query in str(item.value[lang.code]).lower() or query in str(item.key).lower() or query in str(item.group.sub_text + '.' + item.key).lower():
+                if query in str(item.value.get(lang.code, '')).lower() or query in str(item.key).lower() or query in str(item.group.sub_text + '.' + item.key).lower():
                     endlist.append(item)
                 continue
     
