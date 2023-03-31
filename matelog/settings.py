13f91360d61fb6096dfbb63d3df4d963ebac5dbb
MSD <dadamuhames@gmail.com>
2023-03-31 17:06:28 +0500

    Fix emails

--- matelog/settings.py
+++ matelog/settings.py
@@ -224,8 +224,8 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
-EMAIL_HOST_USER = 'matelog13@gmail.com'
-EMAIL_HOST_PASSWORD = 'upixsdugcxubglbp'
+EMAIL_HOST_USER = 'leads@matelogisticss.com'
+EMAIL_HOST_PASSWORD = 'papzxominwbtlnzh'
 
 
 
